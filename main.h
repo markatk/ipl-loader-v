b59e162cc9ff8a554fca9c6e9cfcfd4339a631f3
Markus Grigull <web@grigull.me>
2018-03-23 22:14:19 +0100

    Add base scrithook plugin structure

---
+++ main.h
@@ -0,0 +1,47 @@
+/*
+* File: main.h
+* Date: 23.03.2018
+* Author: MarkAtk
+*
+* MIT License
+*
+* Copyright (c) 2018 IPLLoaderV
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#pragma once
+
+#include <Windows.h>
+
+#define IMPORT __declspec(dllimport)
+
+typedef void(*KeyboardHandler)(DWORD key, WORD repeats, BYTE scanCode, BOOL isExtended, BOOL isWithAlt, BOOL wasDownBefore, BOOL isUpNow);
+
+IMPORT void keyboardHandlerRegister(KeyboardHandler handler);
+IMPORT void keyboardHandlerUnregister(KeyboardHandler handler);
+
+IMPORT void scriptWait(DWORD time);
+IMPORT void scriptRegister(HMODULE module, void(*LP_SCRIPT_MAIN)());
+IMPORT void scriptRegisterAdditionalThread(HMODULE module, void(*LP_SCRIPT_MAIN)());
+IMPORT void scriptUnregister(HMODULE module);
+
+IMPORT void nativeInit(UINT64 hash);
+IMPORT void nativePush64(UINT64 val);
+IMPORT PUINT64 nativeCall();
